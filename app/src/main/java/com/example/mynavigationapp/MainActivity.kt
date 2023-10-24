e201f18e0e28e763b77c444e2cdf2fdd996b4053
3bnaby <mahmoud.mohamad68@gmail.com>
2023-10-24 18:45:19 +0300

    Initial commit

---
+++ app/src/main/java/com/example/mynavigationapp/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.mynavigationapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.example.mynavigationapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.my_toolbar))
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val navController = findNavController(R.id.fragment_host)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+        binding.bottomNavigationView.setupWithNavController(navController)
+
+
+    }
+
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.fragment_host)
+        navController.navigateUp()
+        return true
+
+    }
+}
